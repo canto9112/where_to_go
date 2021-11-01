b7d3d871c006dd883ea351a67c0d9b87418701c4
canto9112 <cantoweb112@gmail.com>
2021-11-01 18:54:16 +0300

    Добавил превью к картинкам

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.safestring import mark_safe
 
 
 class ImageInstanceInline(admin.TabularInline):
     model = Image
+    readonly_fields = ["preview"]
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;">')
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+
     inlines = [ImageInstanceInline]
 
+class ImageAdmin(admin.ModelAdmin):
 
-admin.site.register(Image)
+    readonly_fields = ["preview"]
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;">')
+
+
+admin.site.register(Image, ImageAdmin)
 

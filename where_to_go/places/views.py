9617ba6d5482e4bccae03bd3091d09090fe22d8f
canto9112 <cantoweb112@gmail.com>
2021-11-01 11:59:49 +0300

    Переместил данные из шаблона для просмотра

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Place
 
-# Create your views here.
+
+def serialize_place(place):
+    return {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [place.lng, place.lat]
+          },
+          "properties": {
+            "title": place.title,
+            "placeId": place.pk,
+            "detailsUrl": "/static/json/roofs24.json"
+          }
+        }]
+    }
+
+
+def index(request):
+    all_places = Place.objects.all()
+    context = {
+        'all_places': [serialize_place(place) for place in all_places]
+    }
+    return render(request, "index.html", context)

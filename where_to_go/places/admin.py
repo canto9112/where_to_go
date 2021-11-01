b5050fe3cd23bd07513d4efa6c8047ddd4ecdd49
canto9112 <cantoweb112@gmail.com>
2021-11-01 18:34:40 +0300

    Добавил возможность загружать картинки из Place

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Place, Image
 
-# Register your models here.
-admin.site.register(Place)
+
+class ImageInstanceInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInstanceInline]
+
+
 admin.site.register(Image)
+
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -1,9 +1,12 @@
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
 from places.views import get_detailsUrl, index
 
-urlpatterns = [path('', index),
-               path('<int:place_id>/', get_detailsUrl, name='get_detailsUrl')
-               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', index),
+        path('<int:place_id>/', get_detailsUrl, name='get_detailsUrl')
+        ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
